99b0264197399e52e4ac6cb573cc77c8cf6459ca
Thomas More <tmore1@gmx.com>
2018-11-23 12:32:46 -0500

    Added 'actions' (sync, sshfss, rearchive). Reorganized code accordingly. Updated documentation:  README and FAQ.

--- c14sync.pl
+++ c14sync.pl
@@ -19,9 +19,9 @@ use File::Rsync;
 
 # process command line options
 
-my ($program_name, $program_version) = ('c14sync', 0.1);
+my ($program_name, $program_version) = ('c14sync', 0.2);
 my %opts;
-getopts('rRAv:s:a:t:i:l:c:', \%opts);
+getopts('rRAmuv:s:a:t:i:l:c:', \%opts);
 
 # set defaults for (some) undefined options
 
@@ -39,9 +39,10 @@ if ($conf_file) {
 	foreach ($config->directives) {if (exists $opts_map{$_}) {$opts{$opts_map{$_}} = $config->get($_)} else {print "Undefined configuration file directive '$_' - ignoring.\n";}}
 }
 
+# ensure that universally required parameteres have been supplied
+
 $opts{'s'} || die "No safe name specified - aborting.\n";
 $opts{'a'} || die "No archive name specified - aborting.\n";
-$opts{'l'} || die "No local directory specified - aborting.\n";
 $opts{'t'} || die "No API token specified - aborting.\n";
 
 # setup REST client object
@@ -51,89 +52,131 @@ my $c14_basepath = "$api_basepath/storage/c14";
 my $client = REST::Client->new({host => $api_host});
 $client->addHeader('Authorization', "Bearer $opts{'t'}");
 
-# find archive
+my $archive;
 
-print "Checking whether safe / archive combination \"$opts{'s'} / $opts{'a'}\" exists.\nGetting list of archives ...\n" if $opts{'v'} >= 2;
-my $archive_list = &get("${c14_basepath}/archive");
-print "Got list of archives.\n" if $opts{'v'} >= 2;
-my ($archive, $safe, $flag);
-foreach (@{$archive_list}) {
-	next unless ($_->{'name'} eq $opts{'a'});
-	$archive = &get($_->{'$ref'});
-	$safe = &get($archive->{'safe'}{'$ref'});
-	next unless ($safe->{'name'} eq $opts{'s'});
-	die "Safe / archive combination \"$opts{'s'} / $opts{'a'}\" is not unique - aborting.\n" if $flag;
-	$flag = 1;
+# begin processing
+
+SWITCH: {
+	if ($#ARGV == -1 || $ARGV[0] eq 'sync') {
+		$opts{'l'} || die "No local directory specified - aborting.\n";
+		&find_archive;
+		&open_archive;
+		&rsync;
+		unless ($opts{'r'} or !$opts{'R'}) {
+			&rearchive;
+			&delete_archive;
+		} # we don't rearchive after a reverse rsync, or if told not to do so
+		last SWITCH;
+	}
+	if ($ARGV[0] eq 'sshfs') {
+		$opts{'l'} || die "No local directory specified - aborting.\n";
+		&find_archive;
+		&open_archive;
+		&sshfs;
+		last SWITCH
+	}
+	if ($ARGV[0] eq 'rearchive') {
+		&find_archive;
+		&rearchive;
+		&delete_archive;
+		last SWITCH;
+	}
+	die "Undefined action '$ARGV[0]'. Action must be one of 'sync' [default], 'sshfs', or 'rearchive'.\n";
 }
-die "Safe/archive combination \"$opts{'s'}/$opts{'a'}\" not found - aborting.\n" unless $flag;
-print "Archive found - API URI:\t$archive->{'$ref'}\n" if $opts{'v'} >= 1;
 
-# open archive if it isn't yet open
+exit; # done!
 
-unless ($archive->{'bucket'}) {
-	print "Archive is not open - attempting to open it.\n" if $opts{'v'} >= 1;
-	print "Getting ssh key ids ...\n" if $opts{'v'} >= 2;
-	my $ssh_keys = &get("${api_basepath}/user/key/ssh");
-	print "Got ssh key ids.\n" if $opts{'v'} >= 2;
-	# 'Locations' aren't documented anywhere, and I have no idea what they are, so we just use the first one
-	print "Getting archive locations ...\n" if $opts{'v'} >= 2;	
-	my $locations = &get("$archive->{'$ref'}/location");
-	print "Got archive locations.\n" if $opts{'v'} >= 2;
-	print "Unarchiving ...\n" if $opts{'v'} >= 1;
-	&post("$archive->{'$ref'}/unarchive", encode_json({location_id => ${$locations}[0]{'uuid_ref'}, rearchive => (($opts{'A'} && !$opts{'r'}) ? 1 : 0), protocols => ['SSH'], ssh_keys => [map {$_->{'uuid_ref'}} @{$ssh_keys}]}), {'Content-Type' => 'application/json'});
-	print "Unarchiving successful. Waiting for bucket to be ready ...\n" if $opts{'v'} >= 1;
-	do {
-		sleep 15;
-		$archive = &get($archive->{'$ref'});
-		print "Archive status:\t$archive->{'status'}\n" if $opts{'v'} >= 2;
-	} until ($archive->{'status'} eq 'active');
-	print "Bucket is ready.\n" if $opts{'v'} >= 1;
+# subroutines
+
+sub find_archive {
+	print "Checking whether safe / archive combination \"$opts{'s'} / $opts{'a'}\" exists.\nGetting list of archives ...\n" if $opts{'v'} >= 2;
+	my $archive_list = &get("${c14_basepath}/archive");
+	print "Got list of archives.\n" if $opts{'v'} >= 2;
+	my ($safe, $flag);
+	foreach (@{$archive_list}) {
+		next unless ($_->{'name'} eq $opts{'a'});
+		$archive = &get($_->{'$ref'});
+		$safe = &get($archive->{'safe'}{'$ref'});
+		next unless ($safe->{'name'} eq $opts{'s'});
+		die "Safe / archive combination \"$opts{'s'} / $opts{'a'}\" is not unique - aborting.\n" if $flag;
+		$flag = 1;
+	}
+	die "Safe/archive combination \"$opts{'s'}/$opts{'a'}\" not found - aborting.\n" unless $flag;
+	print "Archive found - API URI:\t$archive->{'$ref'}\n" if $opts{'v'} >= 1;
+}
+
+sub open_archive {
+	unless ($archive->{'bucket'}) {
+		print "Archive is not open - attempting to open it.\n" if $opts{'v'} >= 1;
+		print "Getting ssh key ids ...\n" if $opts{'v'} >= 2;
+		my $ssh_keys = &get("${api_basepath}/user/key/ssh");
+		print "Got ssh key ids.\n" if $opts{'v'} >= 2;
+		# 'Locations' aren't documented anywhere, and I have no idea what they are, so we just use the first one
+		print "Getting archive locations ...\n" if $opts{'v'} >= 2;	
+		my $locations = &get("$archive->{'$ref'}/location");
+		print "Got archive locations.\n" if $opts{'v'} >= 2;
+		print "Unarchiving ...\n" if $opts{'v'} >= 1;
+		&post("$archive->{'$ref'}/unarchive", encode_json({location_id => ${$locations}[0]{'uuid_ref'}, rearchive => (($opts{'A'} && !$opts{'r'}) ? 1 : 0), protocols => ['SSH'], ssh_keys => [map {$_->{'uuid_ref'}} @{$ssh_keys}]}), {'Content-Type' => 'application/json'});
+		print "Unarchiving successful. Waiting for bucket to be ready ...\n" if $opts{'v'} >= 1;
+		do {
+			sleep 15;
+			$archive = &get($archive->{'$ref'});
+			print "Archive status:\t$archive->{'status'}\n" if $opts{'v'} >= 2;
+		} until ($archive->{'status'} eq 'active');
+		print "Bucket is ready.\n" if $opts{'v'} >= 1;
 	}
 	else {print "Archive is open.\n" if $opts{'v'} >= 1}
-
-# setup rsync command
-
-my $bucket = $archive->{'bucket'};
-my $ssh_uri = URI->new($bucket->{'credentials'}[0]{'uri'});
-my ($ssh_host, $ssh_port) = ($ssh_uri->host, $ssh_uri->port);
-my @rsync_options = ('archive' => 1, 'compress' => 1, delete => 1, 'rsh' => "ssh -l $bucket->{'credentials'}[0]{'login'} -p $ssh_port -o 'UserKnownHostsFile = /dev/null' -o 'StrictHostKeyChecking = no'" . (defined $opts{'i'} ? " -i $opts{'i'}" : ''));
-push @rsync_options, ('verbose' => $opts{'v'}) if defined $opts{'v'};
-my $rsync = File::Rsync->new(@rsync_options);
-
-# execute rsync
-
-print "Calling rsync ...\n" if $opts{'v'} >= 1;
-my ($src, $dest) = ($opts{'l'}, "$ssh_host:/buffer");
-($src, $dest) = ("$dest/", $src) if $opts{'r'};
-$rsync->exec('src' => $src, 'dest' => $dest) || die "Rsync failed.\nRsync stderr:\n", $rsync->err, "\n\n";
-print $rsync->out if $opts{'v'} >= 1;
-print "Rsync succeeded.\n" if $opts{'v'} >= 1;
-exit if ($opts{'r'} or !$opts{'R'}); # we don't rearchive after a reverse rsync, or if told not to do so
-
-# rearchive
-
-print "Rearchiving ...\n" if $opts{'v'} >= 1;
-&post("$archive->{'$ref'}/archive", encode_json({duplicates => 1}), {'Content-Type' => 'application/json'});
-print "Rearchiving succeeded.\n" if $opts{'v'} >= 1;
-
-# delete old archive
-
-if (defined $archive->{'size'}) {
-	print "Attempting to delete old archive. Waiting for archive to be ready ...\n" if 	$opts{'v'} >= 1;
-	do {
-		sleep 15;
-		$archive = &get($archive->{'$ref'});
-		print "Archive status:\t$archive->{'status'}\n" if $opts{'v'} >= 2;
-	} until ($archive->{'status'} eq 'active');
-	print "Deleting old archive ...\n" if $opts{'v'} >= 1;
-	&delete($archive->{'$ref'});
-	print "Archive deletion succeeded.\n" if $opts{'v'} >= 1;
-}
-else {
-	print "Parameter 'size' not found in original archive - there is apparently no old archive to delete.\n" if $opts{'v'} >= 1;
 }
 
-exit;
+sub rsync {
+	my $bucket = $archive->{'bucket'};
+	my $ssh_uri = URI->new($bucket->{'credentials'}[0]{'uri'});
+	my ($ssh_host, $ssh_port) = ($ssh_uri->host, $ssh_uri->port);
+	my @rsync_options = ('archive' => 1, 'compress' => 1, delete => 1, 'rsh' => "ssh -l $bucket->{'credentials'}[0]{'login'} -p $ssh_port -o 'UserKnownHostsFile = /dev/null' -o 'StrictHostKeyChecking = no'" . (defined $opts{'i'} ? " -i $opts{'i'}" : ''));
+	push @rsync_options, ('verbose' => $opts{'v'}) if defined $opts{'v'};
+	my $rsync = File::Rsync->new(@rsync_options);
+	print "Calling rsync ...\n" if $opts{'v'} >= 1;
+	my ($src, $dest) = ($opts{'l'}, "$ssh_host:/buffer");
+	($src, $dest) = ("$dest/", $src) if $opts{'r'};
+	$rsync->exec('src' => $src, 'dest' => $dest) || die "Rsync failed.\nRsync stderr:\n", $rsync->err, "\n\n";
+	print $rsync->out if $opts{'v'} >= 1;
+	print "rsync succeeded.\n" if $opts{'v'} >= 1;
+}
+
+sub sshfs {
+	my $bucket = $archive->{'bucket'};
+	my $ssh_uri = URI->new($bucket->{'credentials'}[0]{'uri'});
+	my ($ssh_host, $ssh_port) = ($ssh_uri->host, $ssh_uri->port);
+	print "Calling sshfs ...\n" if $opts{'v'} >= 1;
+	my $sshfs = "sshfs $bucket->{'credentials'}[0]{'login'}\@$ssh_host:/buffer $opts{'l'} -p $ssh_port -C -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" . (defined $opts{'i'} ? " -o IdentityFile=$opts{'i'} " : '' . (defined $opts{'d'} ? " -d" : ''));
+	print "$sshfs\n";
+	#$sshfs .= " -d" if $opts{'v'} >=2;
+	!system ($sshfs) || die "sshfs failed!\n";
+	print "sshfs succeeded - \"$opts{'s'} / $opts{'a'}\" is mounted on $opts{'l'}\nUse \"fusermount -u $opts{'l'}\" to umount (optionally followed by \"$program_name rearchive\" if appropriate).\n" if $opts{'v'} >= 1;
+}
+
+sub rearchive {
+	print "Rearchiving ...\n" if $opts{'v'} >= 1;
+	&post("$archive->{'$ref'}/archive", encode_json({duplicates => 1}), {'Content-Type' => 'application/json'});
+	print "Rearchiving succeeded.\n" if $opts{'v'} >= 1;
+}
+
+sub delete_archive {
+	if (defined $archive->{'size'}) {
+		print "Attempting to delete old archive. Waiting for archive to be ready ...\n" if 	$opts{'v'} >= 1;
+		do {
+			sleep 15;
+			$archive = &get($archive->{'$ref'});
+			print "Archive status:\t$archive->{'status'}\n" if $opts{'v'} >= 2;
+		} until ($archive->{'status'} eq 'active');
+		print "Deleting old archive ...\n" if $opts{'v'} >= 1;
+		&delete($archive->{'$ref'});
+		print "Archive deletion succeeded.\n" if $opts{'v'} >= 1;
+	}
+	else {
+		print "Parameter 'size' not found in original archive - there is apparently no old archive to delete.\n" if $opts{'v'} >= 1;
+	}
+}
 
 # API HTTP verb subroutines
 
